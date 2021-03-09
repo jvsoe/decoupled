4039b1fd0f0bfd156416cca456d44a8ecade62ec
Jacob Vedersø <jacob.vsoe@gmail.com>
2021-03-09 12:14:44 +0100

    Webpack setup for reactstrap

--- juggle/frontend/src/index.js
+++ juggle/frontend/src/index.js
@@ -1,1 +1,2 @@
 import App from "./components/App";
+import 'bootstrap/dist/css/bootstrap.min.css'
--- juggle/frontend/webpack.config.js
+++ juggle/frontend/webpack.config.js
@@ -1,12 +1,55 @@
+const path = require('path')
+console.log('__dirname', __dirname)
 module.exports = {
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.js$/,
+  //       exclude: /node_modules/,
+  //       use: {
+  //         loader: "babel-loader"
+  //       }
+  //     }
+  //   ]
+  // },
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.js$/,
+  //       exclude: /node_modules/,
+  //       use: {
+  //         loader: 'babel-loader',
+  //         options: {
+  //           presets: ['@babel/env', "@babel/react"],
+  //         }
+  //       }
+  //     }
+  //   ]
+  // },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(jsx|js)$/,
+        include: path.resolve(__dirname, 'src'),
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                "targets": "defaults"
+              }],
+              '@babel/preset-react'
+            ]
+          }
+        }],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
       }
     ]
   }
